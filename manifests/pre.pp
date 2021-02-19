e73ca6ac3bdc0542b9bc2d226a5d15fafec1ade6
Peter Crowther <peter.crowther@melandra.com>
2021-02-19 10:53:55 +0000

    Add provenance to comments on pre rules

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -4,22 +4,22 @@ class firewall_framework::pre {
   Firewall {
     require => undef,
   }
-  firewall { '000 accept all icmp':
+  firewall { '000 accept all icmp (powelectrics/firewall_framework)':
     proto  => 'icmp',
     action => 'accept',
   }
-  firewall { '001 accept all to lo interface':
+  firewall { '001 accept all to lo interface (powelectrics/firewall_framework)':
     proto   => 'all',
     iniface => 'lo',
     action  => 'accept',
   }
-  firewall { '002 reject local traffic not on loopback interface':
+  firewall { '002 reject local traffic not on loopback interface (powelectrics/firewall_framework)':
     iniface     => '! lo',
     proto       => 'all',
     destination => '127.0.0.1/8',
     action      => 'reject',
   }
-  firewall { '003 accept related established rules':
+  firewall { '003 accept related established rules (powelectrics/firewall_framework)':
     proto  => 'all',
     state  => ['RELATED', 'ESTABLISHED'],
     action => 'accept',
