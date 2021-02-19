ae84d0f44c6ecc805362360a7a09ecffd2a9969d
Peter Crowther <peter.crowther@melandra.com>
2021-02-19 10:54:24 +0000

    Add provenance to firewall rules in post

--- manifests/post.pp
+++ manifests/post.pp
@@ -1,7 +1,7 @@
 # Configuration applied at the end of firewall update, after all other rules.
 # From https://puppet.com/docs/puppet/7.4/quick_start_firewall.html
 class firewall_framework::post {
-  firewall { '999 drop all':
+  firewall { '999 drop all (powelectrics/firewall_framework)':
     proto  => 'all',
     action => 'drop',
     before => undef,
