b2f784832f323ae6ed844b4a9f5fd7eca466200e
Faye <fayegibbins12@gmail.com>
2022-12-19 11:12:06 +0000

    Fix class name

--- manifests/tcp_udp_allow.pp
+++ manifests/tcp_udp_allow.pp
@@ -7,12 +7,12 @@ define firewall_framework::tcp_udp_allow (
   $label=$name
   )
   {
-    my_fw::tcp_allow {"tcp_allow_${name}":
+    firewall_framework::tcp_allow {"tcp_allow_${name}":
       port     => $port,
       priority => $priority,
       label    => $label,
     }
-    my_fw::udp_allow {"udp_allow_${name}":
+    firewall_framework::udp_allow {"udp_allow_${name}":
       port     => $port,
       priority => $priority,
       label    => $label,
